4e6c53eb9fd0e0d182d99bec3b8587ca48490a2f
diommyliel <thomascoudurie@gmail.com>
2023-12-21 18:11:57 -0500

    fix is in value set

--- src/validator/value_validator.py
+++ src/validator/value_validator.py
@@ -4,11 +4,15 @@ validators to be applied to values in column
 Ex: is_greater_than, is_in_value_set, ...
 '''
 import typing as t
+from polars import Series
 
 
-def is_greater_than(a: float):
+T = t.Callable[[Series], Series]
+
+
+def is_greater_than(a: float) -> T:
     return lambda x: x > a
 
 
-def is_in_value_set(s: t.Set):
-    return lambda x: x in s
+def is_in_value_set(s: t.Set) -> T:
+    return lambda x: x.is_in(s)
